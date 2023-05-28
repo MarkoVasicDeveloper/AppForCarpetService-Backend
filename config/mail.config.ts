2a90a05d33d9dbbfc7ffeb51a35397aa3e4cd988
MarkoVasicDeveloper <84353415+MarkoVasicDeveloper@users.noreply.github.com>
2023-05-28 22:39:56 +0200

    Delete config directory

--- config/mail.config.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable prettier/prettier */
-export const mailConfig = {
-    hostname: 'smtp.google.com',
-    username: 'washer',
-    password: 'dubinskopranje'
-}
--- config/react.config.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable prettier/prettier */
-export const ReactConfig = {
-  destination: "../build",
-  prefix: "/",
-};
